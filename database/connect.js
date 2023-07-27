7f0e324a68435b31acf0f6902688fccf82cd660f
fhoinaski <sukinodoncai@gmail.com>
2023-07-27 12:46:36 -0300

    correçãoes

--- database/connect.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const dbConfig = require('../config/db.config');
-
-const connect = new Sequelize(dbConfig);
-
-//exporta o modulo de conexão
-module.exports = {connect};
